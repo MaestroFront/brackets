87557308f263a6803d7c4635e7dda34f3b7bc61b
Maestro <akievaleksndr@mail.ru>
2022-07-14 13:45:19 +0500

    feat: upgrade code, add forEach()

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-module.exports = function check(str, bracketsConfig) {
-    do {
-        str = str.replace('()', '');
-        str = str.replace('[]', '');
-        str = str.replace('{}', '');
-        str = str.replace('||', '');
-        str = str.replace('12', '');
-        str = str.replace('34', '');
-        str = str.replace('56', '');
-        str = str.replace('77', '');
-        str = str.replace('88', '');
-    } while (
-        str.includes('()') || str.includes('[]') ||  str.includes('{}') || 
-        str.includes('||') || str.includes('12') ||  str.includes('34') || 
-        str.includes('56') || str.includes('77') ||  str.includes('88') )
-    const result = (item) => item > 0 ? false : true;
-    return result(str.length);
+const check = (str) => {
+    const array = ['()', '[]', '{}', '||', '12', '34', '56', '77', '88'];
+    for (let i = 0; i < array.length; i++){
+        do {
+            array.forEach((item => str = str.replace(item, '')))
+        } while (str.includes(array[i]))
+    }
+    return str.length > 0 ? false : true;
 }
